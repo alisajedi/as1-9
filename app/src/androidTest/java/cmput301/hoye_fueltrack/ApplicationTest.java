6f9b8723b47380bdf2e7c595b161001d360cd274
stuarthoye <hoye@ualberta.ca>
2016-02-01 11:36:30 -0700

    Add tests for FuelTracker class

--- app/src/androidTest/java/cmput301/hoye_fueltrack/ApplicationTest.java
+++ app/src/androidTest/java/cmput301/hoye_fueltrack/ApplicationTest.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
 
+import java.util.ArrayList;
+
 
 public class ApplicationTest extends ActivityInstrumentationTestCase2 {
     public ApplicationTest() {
@@ -43,5 +45,29 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2 {
         log1.setFuel_unit_cost(newCost);
         assertEquals((amount * newCost) / 100, log1.getFuel_total_cost());
     }
+
+    public void testFuelTracker() {
+        FuelTracker tracker = new FuelTracker();
+        ArrayList<Log> logs = new ArrayList<Log>();
+        String date = "2016-01-18";
+        String station = "Costco";
+        double odo = 200123.5;
+        String type = "regular";
+        double amount = 40.234;
+        double cost = 79.4;
+        Log log1 = new Log(date, station, odo, type, amount, cost);
+
+        tracker.addLog(log1);
+        logs.add(log1);
+        assertEquals(logs, tracker.getLogs());
+
+        Log log2 = new Log("2016-01-19", station, odo, type, amount, cost);
+        tracker.addLog(log2);
+        logs.add(log2);
+        assertEquals(logs, tracker.getLogs());
+
+        
+    }
+
 }
 
